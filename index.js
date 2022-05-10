52406c96e66032b64fba0b15fb178afaa2fd0eb6
JamesNg <njames15@gmail.com>
2022-05-10 16:37:44 -0400

    MVP done, click and mouseover events

--- index.js
+++ index.js
@@ -1,27 +1,106 @@
-// COR5 Emojis & spirits - Gin, Vodka, Tequila, Rum, Whiskey
-// Add beer afterwards if core**
-// const grinWithBigEyes = '🙋' ;
-// console.log(grinWithBigEyes); 
-// const h1 = document.querySelector('h1');  
-// h1.innerText = grinWithBigEyes;
-
-// console.log(grinWithBigEyes);
-fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mojito')
-.then(res => res.json())
-.then((data) => {
-    console.log(data);
-    console.log(data.drinks);  //data = object, drinks = key, array of drink info = value (want!)
-    getDrinks(data);
-})
-
+//Div tags
 const emojiDiv = document.querySelector('#emoji');
 const cocktailDiv = document.querySelector('#cocktail');
 const detailDiv = document.querySelector('#description');
 
-let emoji = document.createElement('img');
-emoji.src = 'emojis/Happy Emoji.png'
-emojiDiv.append(emoji);
+//Emoji tags
+const happy = document.getElementById('happy');
+const party = document.getElementById('party');
+const angry = document.getElementById('angry');
+const sad = document.getElementById('sad');
+const smirk = document.getElementById('smirk');
 
+//Click Event
+function clickEmojis() {
+    happy.addEventListener('click', () => {
+        cocktailDiv.innerHTML = '';
+        fetchMojitos();
+    });
+    party.addEventListener('click', () => {
+        cocktailDiv.innerHTML = '';
+        fetchLongIslands();
+    });
+    angry.addEventListener('click', () => {
+        cocktailDiv.innerHTML = '';
+        fetchMartinis()
+    });
+    sad.addEventListener('click', () => {
+        cocktailDiv.innerHTML = '';
+        fetchDaiquiris();
+    });
+    smirk.addEventListener('click', () => {
+        cocktailDiv.innerHTML = '';
+        fetchMargaritas()
+    });
+}
+
+clickEmojis();
+
+
+
+function fetchMojitos() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mojito')
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data);
+        console.log(data.drinks);  //data = object, drinks = key, array of drink info = value (want!)
+        getDrinks(data);
+    })
+}
+
+
+//fetch long island ice teas
+function fetchLongIslands() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Long')
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data);
+        console.log(data.drinks);  //data = object, drinks = key, array of drink info = value (want!)
+        getDrinks(data);
+    })
+}
+
+
+//fetch martinis - smirk
+function fetchMartinis() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Martini')
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data);
+        console.log(data.drinks);  //data = object, drinks = key, array of drink info = value (want!)
+        getDrinks(data);
+    })
+}
+
+
+//fetch daiquiris
+function fetchDaiquiris() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Daiquiri')
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data);
+        console.log(data.drinks);  //data = object, drinks = key, array of drink info = value (want!)
+        getDrinks(data);
+    })
+}
+
+
+//fetch margaritas
+function fetchMargaritas() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data);
+        console.log(data.drinks);  //data = object, drinks = key, array of drink info = value (want!)
+        getDrinks(data);
+    })
+}
+
+// fetchMargaritas();
+
+
+
+//Adds drink images
 function getDrinks(data) {
     
     for (const item of data.drinks) { //data.drinks -> array of 4
@@ -31,7 +110,7 @@ function getDrinks(data) {
         drinkImg.src = item.strDrinkThumb;
         cocktailDiv.append(drinkImg); //appends drink image to the cocktail DIV
 
-        
+        //Mouseover Event
         drinkImg.addEventListener('mouseover', (e) => {
             
             // console.log(e);
@@ -43,7 +122,7 @@ function getDrinks(data) {
 
             //add Cocktail name, description, recipe, where to get it
             let drinkRecipe = document.createElement('p');
-            drinkRecipe.innerText = item.strInstructions;
+            drinkRecipe.innerText = item.strDrink;
             console.log(drinkRecipe);
             detailDiv.append(drinkRecipe);
 
