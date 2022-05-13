c9975c4809c4e0c37ee06f3ecb846f7b6d38c320
JamesNg <njames15@gmail.com>
2022-05-13 11:19:00 -0400

    final

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //Welcome Prompt
-function welcome() {
-
-    let greetings = prompt('Are you 21 or older?');
-    if (greetings == 'yes') {
-        alert('Welcome!');
-        clickNDrink();
-    } else if (greetings == 'no') {
+function welcome() { 
+    
+    let greetings = prompt('Are you under 21??? We can get you in. \u{1F92B}');
+    if (greetings === 'no') {
+        alert("Checking to see if you are lying...");
+        alert('Ok, welcome to CLICK N\' DRINK!');
+    } else if (greetings === 'yes') {
         getOut();
     } else {
         alert('Please enter "yes" or "no".');
@@ -13,7 +13,7 @@ function welcome() {
     }
 };
 
-// welcome();
+welcome();
 
 
 
@@ -37,38 +37,49 @@ function clickNDrink() {
     let happyEmoji = document.createElement('img');
     happyEmoji.id = 'happiness';
     happyEmoji.setAttribute('class', 'hidden');
+    happyEmoji.style.cssText = 'margin-left: 69px;';
     happyEmoji.src = "emojis/Happy Emoji.png";
 
     let partyEmoji = document.createElement('img');
     partyEmoji.id = 'partayy';
     partyEmoji.setAttribute('class', 'hidden');
+    partyEmoji.style.cssText = 'margin-left: 69px;';
     partyEmoji.src = "emojis/Party Face Emoji.png";
     
     let angryEmoji = document.createElement('img');
     angryEmoji.id = 'anger';
     angryEmoji.setAttribute('class', 'hidden');
+    angryEmoji.style.cssText = 'margin-left: 69px;';
     angryEmoji.src = "emojis/Very Angry Emoji.png";
 
     let sadEmoji = document.createElement('img');
     sadEmoji.id = 'sadness';
     sadEmoji.setAttribute('class', 'hidden');
+    sadEmoji.style.cssText = 'margin-left: 69px;';
     sadEmoji.src = "emojis/Very Sad Emoji.png";
 
     let smirkEmoji = document.createElement('img');
     smirkEmoji.id = 'smirky';
     smirkEmoji.setAttribute('class', 'hidden');
+    smirkEmoji.style.cssText = 'margin-left: 69px;';
     smirkEmoji.src = "emojis/Smirk Face Emoji.png";
     
     //Text for picking a drink
     let pickADrink = document.createElement('p');
-    pickADrink.innerText = 'Pick a drink:';
-    pickADrink.style.cssText = 'text-align = center; font: 30px Arial; font-weight: bold';
+    pickADrink.innerText = 'Drink recommendations just for you: (choose one)';
+    pickADrink.style.cssText = 'text-align = center; font: 30px Comic Sans MS; font-weight: bold';
     
 
     function clearHTML() {
         cocktailDiv.innerHTML = '';
         detailDiv.innerHTML = '';
         feedEmojiDiv.innerHTML = '';
+        dragAcross.className = 'hidden';
+        happyEmoji.setAttribute('class', 'hidden');
+        partyEmoji.setAttribute('class', 'hidden');
+        angryEmoji.setAttribute('class', 'hidden');
+        sadEmoji.setAttribute('class', 'hidden');
+        smirkEmoji.setAttribute('class', 'hidden');
     };
 
 
@@ -186,7 +197,7 @@ function clickNDrink() {
     
     let mood = document.createElement('p');
     mood.innerText = 'This is you right now: \n (hover below)' ;
-    mood.style.cssText = 'font: 35px Arial, sans-serif; text-align: center; font-weight: bold;'
+    mood.style.cssText = 'font: 35px Comic Sans MS; text-align: center; font-weight: bold; margin-top: 0px; width: 400px;'
     
 
     //Add drink images with forEach method
@@ -207,7 +218,8 @@ function clickNDrink() {
 
                 //adds cocktail name
                 let drinkName = document.createElement('p');
-                drinkName.innerText = item.strDrink;
+                drinkName.innerText = item.strDrink.toUpperCase();
+                drinkName.style.cssText = 'font: 35px Comic Sans MS; font-weight: bold;'; 
 
                 //adds cocktail image
                 let drinkOfChoice = document.createElement('img');
@@ -241,18 +253,18 @@ function clickNDrink() {
 
     const dragNdrop = document.getElementById('dragndrop');
     let dragAcross = document.createElement('p');
-    dragAcross.innerText = 'Give me the drink \n >>>>>>>>>>>>>>>>>>>>';
+    dragAcross.innerText = 'Give me the drink \n >>>>>>>>>>>>>>>>>>>>>>>>>> \n (drag and drop)';
     dragAcross.className = 'hidden';
-    dragAcross.style.cssText = 'text-align: center; font-size: 20px;';
+    dragAcross.style.cssText = 'text-align: center; font: 25px Comic Sans MS; margin-top: 250px;';
     
     let finalMsg = document.createElement('p');
     finalMsg.className = 'hidden';
-    finalMsg.innerText = 'This is you afterwards:';
-    finalMsg.style.cssText = 'text-align: center; font: 30px Arial, sans-serif; font-weight: bold;';
+    finalMsg.innerText = 'This is you afterwards:'.toUpperCase();
+    finalMsg.style.cssText = 'text-align: center; font: 25px Comic Sans MS; font-weight: bold; margin: 25px 0px 0px 50px; height: 30px; width: 400px;';
 
     let ecstaticEmoji = document.createElement('img');
     // ecstaticEmoji.id = 'ecstatic';
-    ecstaticEmoji.style.cssText = 'text-align: center; display: center; height: 350px;';
+    ecstaticEmoji.style.cssText = 'display: center; height: 500px; width: auto;';
     ecstaticEmoji.src = 'emojis/ecstatic.png';
     ecstaticEmoji.className = 'hidden';
 
@@ -321,82 +333,98 @@ function clickNDrink() {
             // move dragged elem to the selected drop target
             if ( e.target.id == "happiness" ) {
                 
-                detailDiv.innerHTML = '';
-                feedEmojiDiv.innerHTML = '';
-                // feedEmojiDiv.style.padding = '100px 200px 100px 0px';
-                // dragNdrop.style.padding = '50px 200px 50px 200px';
-                // detailDiv.style.padding = '100px 200px 100px 100px';
-                dragAcross.hidden = true;
-                finalMsg.className = '';
-                ecstaticEmoji.className = '';
+                // detailDiv.innerHTML = '';
+                // feedEmojiDiv.innerHTML = '';
+                // dragNdrop.style.padding = '50px 50px 50px 50px';
+                detailDiv.remove();
+                feedEmojiDiv.remove();
+                dragNdrop.style.padding = '50px 200px 50px 200px';
+                
+                dragAcross.innerText = 'Chugging.....';
+                document.body.style.cursor = 'progress';
+                setTimeout( () => {
+                    dragAcross.hidden = true;
+                    finalMsg.className = '';
+                    ecstaticEmoji.className = '';
+                    document.body.style.cursor = '';
+                    dragNdrop.style.cssText = 'margin-right: 200px;';
+                }, 2000);
+
 
             } else if ( e.target.id == "sadness" ) {
                 
-                detailDiv.innerHTML = '';
-                feedEmojiDiv.innerHTML = '';
-                // feedEmojiDiv.style.padding = '100px 200px 100px 0px';
-                // dragNdrop.style.padding = '50px 200px 50px 200px';
-                // detailDiv.style.padding = '100px 200px 100px 100px';
-                dragAcross.hidden = true;
-                finalMsg.className = '';
-                ecstaticEmoji.className = '';
+                detailDiv.remove();
+                feedEmojiDiv.remove();
+                dragNdrop.style.padding = '50px 200px 50px 200px';
+
+                dragAcross.innerText = 'Chugging.....';
+                document.body.style.cursor = 'progress';
+                setTimeout( () => {
+                    dragAcross.hidden = true;
+                    finalMsg.className = '';
+                    ecstaticEmoji.className = '';
+                    document.body.style.cursor = '';
+                    dragNdrop.style.cssText = 'margin-right: 200px;';
+                }, 2000);
                 
             } else if ( e.target.id == "anger" ) {
                 
-                detailDiv.innerHTML = '';
-                feedEmojiDiv.innerHTML = '';
-                feedEmojiDiv.style.padding = '100px 200px 100px 0px';
+                detailDiv.remove();
+                feedEmojiDiv.remove();
                 dragNdrop.style.padding = '50px 200px 50px 200px';
-                detailDiv.style.padding = '100px 200px 100px 100px';
-                dragAcross.hidden = true;
-                finalMsg.className = '';
-                ecstaticEmoji.className = '';
+
+                dragAcross.innerText = 'Chugging.....';
+                document.body.style.cursor = 'progress';
+                setTimeout( () => {
+                    dragAcross.hidden = true;
+                    finalMsg.className = '';
+                    ecstaticEmoji.className = '';
+                    document.body.style.cursor = '';
+                    dragNdrop.style.cssText = 'margin-right: 200px;';
+                }, 2000);
                 
             } else if ( e.target.id == "smirky" ) {
                 
-                detailDiv.innerHTML = '';
-                feedEmojiDiv.innerHTML = '';
-                feedEmojiDiv.style.padding = '100px 200px 100px 0px';
+                detailDiv.remove();
+                feedEmojiDiv.remove();
                 dragNdrop.style.padding = '50px 200px 50px 200px';
-                detailDiv.style.padding = '100px 200px 100px 100px';
-                dragAcross.hidden = true;
-                finalMsg.className = '';
-                ecstaticEmoji.className = '';
+
+                dragAcross.innerText = 'Chugging.....';
+                document.body.style.cursor = 'progress';
+                setTimeout( () => {
+                    dragAcross.hidden = true;
+                    finalMsg.className = '';
+                    ecstaticEmoji.className = '';
+                    document.body.style.cursor = '';
+                    dragNdrop.style.cssText = 'margin-right: 200px;';
+                }, 2000);
                 
             } else if ( e.target.id == "partayy" ) {
                 
-                detailDiv.innerHTML = '';
-                feedEmojiDiv.innerHTML = '';
-                feedEmojiDiv.style.padding = '100px 200px 100px 0px';
+                detailDiv.remove();
+                feedEmojiDiv.remove();
                 dragNdrop.style.padding = '50px 200px 50px 200px';
-                detailDiv.style.padding = '100px 200px 100px 100px';
-                dragAcross.hidden = true;
-                finalMsg.className = '';
-                ecstaticEmoji.className = '';
+                
+                dragAcross.innerText = 'Chugging.....';
+                document.body.style.cursor = 'progress';
+                setTimeout( () => {
+                    dragAcross.hidden = true;
+                    finalMsg.className = '';
+                    ecstaticEmoji.className = '';
+                    document.body.style.cursor = '';
+                    dragNdrop.style.cssText = 'margin-right: 200px;';
+                }, 2000);
                 
             }
         }, false);
 
     }
-    // setTimeout( ()=> {
-                //     let anotherDrink = prompt('Want another drink?');
-                //     if (anotherDrink === 'yes') {
-                //         alert('What would you like?');
-                //     } else if (anotherDrink === 'no') {
-                //         alert('Ok, enjoy your night!');
-                //     } else {
-                //         prompt('Yes or no?');
-                //     }
-                // }, 1500);
-            
+           
     feedDrinkDragNDrop();
 
 }
 
 clickNDrink();
-
-
-
 
 function getOut() {
 
@@ -405,16 +433,13 @@ function getOut() {
     let div = document.createElement('div');
     let scolding = document.createElement('img');
     scolding.src = 'emojis/scolding.png';
+    scolding.style.height = '400px';
     let msg = document.createElement('h2');
     msg.innerText = "Get outta' here or we'll tell your mom!";
     msg.style.textAlign = 'center';
 
     div.append(scolding);
-    div.style.cssText = 'text-align: center; padding-top: 50px;';
+    div.style.cssText = 'text-align: center; padding-top: 125px;';
     document.body.append(div, msg);
 
 }
-
-
-//Upon dropping the drink on the face, clear innerHTML of #description and #feed-emoji divs, 
-//and append the 'ecstatic' smiley at dragndrop div with setTimeout method for 3000ms (3sec)
